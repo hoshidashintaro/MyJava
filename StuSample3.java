faaf8bbee04de7f49ff25f93febf96599a99489a
hoshidashintaro <shin03ho09@gmail.com>
2024-06-13 07:48:36 +0900

    課題4-3を作成完了

--- StuSample3.java
+++ StuSample3.java
@@ -1,27 +1,7 @@
-/*
- * 内容：オーバーライド
- * オーバーライド：サブクラスでスーパークラスのメソッドを再定義すること
- * オーバーライドの元の意味：上書きする
- * 条件：戻り値の型、メソッド名、引数の型と数、を合わせる（同じにする）
- * 記述例
- * class Person {
- *     void display(){}
- * }
- * class Student extends Person { ←こちらが出力される
- *     void display(){}
- * }
- * オーバーロード：１つのクラスの中で同じ名前のメソッドを複数定義できる
-*/
 //実行用クラス
-public class StuSample2 {
+public class StuSample3 {
     public static void main(String[] args) {
-        Student2 stu = new Student2();
-        //スーパークラスのメソッド
-        //setNameは、public指定のため呼び出せる
-        stu.setName("菅原");
-        //サブクラスのメソッド
-        //setStuNoは、public指定のため呼び出せる
-        stu.setStuNo(1);
+        Student3 stu = new Student3("菅原");
         stu.display();
     }
 }
--- Student3.java
+++ Student3.java
@@ -1,30 +1,17 @@
-/*
- * 内容：オーバーライド
- * オーバーライド：サブクラスでスーパークラスのメソッドを再定義すること
- * オーバーライドの元の意味：上書きする
- * 条件：戻り値の型、メソッド名、引数の型と数、を合わせる（同じにする）
- * 記述例
- * class Person {
- *     void display(){}
- * }
- * class Student extends Person { ←こちらが出力される
- *     void display(){}
- * }
- * オーバーロード：１つのクラスの中で同じ名前のメソッドを複数定義できる
-*/
 //子クラス
-public class Student2 extends Person2 {
+public class Student3 extends Person3 {
     private int stuNo;
 
-    public void setStuNo(int s) {
-        stuNo = s;
+    public Student3(String name) {
+        this(name, 999);
     }
-    //元の記述：public void displayStuNo()
+    public Student3(String name, int stuNo) {
+        super(name);
+        this.stuNo = stuNo;
+    }
+
     public void display() {
-        //追記：System.out.println("名前：" + getName);
-        //nameはprivate指定しているため、getName()出ないと呼び出せない
-        System.out.println("名前：" + getName());
-        //stuNoはprivate指定されているが同じクラス内なので使うことができる
+        super.display();
         System.out.println("学籍番号：" + stuNo);
     }
 }
