50d2e7abc8f8f25396e47bbe56fea5aff5970515
hoshidashintaro <shin03ho09@gmail.com>
2024-06-15 10:45:31 +0900

    課題4-4の各ファイルに学習内容を記載：１回目

--- DameStudent.java
+++ DameStudent.java
@@ -1,3 +1,16 @@
+/*
+ * 内容：finalについて
+ * ・クラス、メンバ変数、メソッドにつけられる修飾子 "これで最後という意味"
+ * クラス：継承ができなくなる
+ * final class Student{}
+ *  → class DameStudent extends Student{} この場合は、Studentクラスの継承ができない
+ * メンバ変数：代入ができなくなる → 定数が作れる（定数は大文字にする）慣習として
+ * final int BASE_NO = 1000;
+ *  → BASE_NO = 200; この場合、BASE_NOは2000で上書きできなくなる
+ * メソッド：オーバーライドができなくなる
+ *  → final void buckup() {}
+ *    サブクラスで再定義できなくなる
+ */
 public class DameStudent extends Student4 {
     DameStudent(String name, int stuNo) {
         super(name, stuNo);
--- Person4.java
+++ Person4.java
@@ -1,5 +1,15 @@
 /*
- * 内容：this superについて
+ * 内容：finalについて
+ * ・クラス、メンバ変数、メソッドにつけられる修飾子 "これで最後という意味"
+ * クラス：継承ができなくなる
+ * final class Student{}
+ *  → class DameStudent extends Student{} この場合は、Studentクラスの継承ができない
+ * メンバ変数：代入ができなくなる → 定数が作れる（定数は大文字にする）慣習として
+ * final int BASE_NO = 1000;
+ *  → BASE_NO = 200; この場合、BASE_NOは2000で上書きできなくなる
+ * メソッド：オーバーライドができなくなる
+ *  → final void buckup() {}
+ *    サブクラスで再定義できなくなる
  */
 //親クラス（スーパークラス）
 public class Person4 {
